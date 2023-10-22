312e68866c56b79af889e95f073bfa2d037968c6
Adam <7889445+DMNerd@users.noreply.github.com>
2023-10-23 00:39:25 +0200

    Thefuck fixed, added check for terminator as well

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ paru -Qs stow || paru -S stow
 #Terminator
 paru -Qs terminator || paru -S terminator
 
+if  test -d $HOME/.config/terminator; then
+    rm -rf $HOME/.config/terminator
+fi
+mkdir $HOME/.config/terminator
+
 stow -t $HOME/.config/terminator -D terminator 
 stow -t $HOME/.config/terminator -v terminator
 
@@ -12,11 +17,12 @@ paru -Qs zsh || paru -S zsh
 
 paru -Qs fastfetch || paru -S fastfetch
 paru -Qs bpytop || paru -S bpytop
-dparu -Qs lsd || paru -S lsd
+paru -Qs lsd || paru -S lsd
+paru -Qs thefuck || paru -S thefuck
 
 if  test -f $HOME/.zshrc; then
     rm $HOME/.zshrc
 fi
-    
+
 stow -t $HOME -D zsh
 stow -t $HOME -v zsh
