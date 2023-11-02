1c1b6dd0c6c0033b3767197f67d09a5fa425b6cd
DMNerd <7889445+DMNerd@users.noreply.github.com>
2023-11-02 22:45:06 +0100

    Switcharoo

--- install.sh
+++ install.sh
@@ -30,14 +30,14 @@ if ! test -d $HOME/.zsh/zsh-autosuggestions; then
     git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.zsh/zsh-autosuggestions"
 fi
 
+if test -f $HOME/.zshrc; then
+    rm $HOME/.zshrc
+fi
+
 sudo dnf install fastfetch
 sudo dnf install bpytop
 sudo dnf install exa
 sudo dnf install thefuck
-
-if test -f $HOME/.zshrc; then
-    rm $HOME/.zshrc
-fi
 
 stow -t $HOME -D zsh
 stow -t $HOME -v zsh
